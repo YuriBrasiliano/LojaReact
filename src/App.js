d23d63e5ffd89692165d99db87920b5b53895c64
Yuri Brasiliano <yuribrasiliano37@gmail.com>
2023-08-10 20:12:05 -0300

    Add valor ao cart e somando total

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   };
 
   const removeFromCart = (product) => {
-    if (product.quantity > 0) {
+    if (product.quantity >= 1) {
       const updatedCart = cartItems.map((item) =>
         item.id === product.id ? { ...item, quantity: item.quantity - 1 } : item
       );
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,11 +2,22 @@ import React from 'react';
 
 function Cart({ cartItems, removeFromCart }) {
   const handleRemoveFromCart = (item) => {
-    if (item.quantity > 1) {
+    if (item.quantity >= 1) {
       removeFromCart({ ...item, quantity: item.quantity - 1 });
     } else {
       removeFromCart(item);
     }
+  };
+
+  const calculateTotalPrice = (item) => {
+    return (item.price * item.quantity).toFixed(2);
+  };
+
+  const calculateCartTotal = () => {
+    const total = cartItems.reduce((accumulator, item) => {
+      return accumulator + parseFloat(calculateTotalPrice(item));
+    }, 0);
+    return total.toFixed(2);
   };
 
   return (
@@ -15,18 +26,21 @@ function Cart({ cartItems, removeFromCart }) {
       <ul>
         {cartItems.map((item) => (
           <li key={item.id}>
-<p className='itemInfoCart'>
-  <div className='itemName'>Produto: </div> &nbsp; {item.name}
-  <div className='itemQuantidade'>Quantidade: {item.quantity}</div>
-  <div className='itemPreco'>R$ </div> &nbsp; {item.price}
-</p>
 
+<p className='itemInfoCart'>
+<div className='itemName'>Produto: </div> &nbsp; {item.name}
+<div className='itemQuantidade'>Quantidade: {item.quantity}</div>
+<div className='itemPreco'>R$ </div> &nbsp; {calculateTotalPrice(item)}
+</p>
             <button onClick={() => handleRemoveFromCart(item)}>Remover</button>
           </li>
         ))}
       </ul>
+      <p className='valorCart'>Total do Carrinho: <div className='valor'>R$ {calculateCartTotal()}</div></p>
     </div>
   );
 }
 
 export default Cart;
+
+
