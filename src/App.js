7513f0ba213e9d494af8c0a54d3beead174c6ef4
Yuri Brasiliano <yuribrasiliano37@gmail.com>
2023-08-10 21:13:13 -0300

    Add tela com descricao do produto

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './styles/index.css';
 
 function App() {
   const [cartItems, setCartItems] = useState([]);
+  const [showDetails, setShowDetails] = useState(false);
 
   const addToCart = (product) => {
     const existingItem = cartItems.find((item) => item.id === product.id);
@@ -33,8 +34,13 @@ function App() {
   return (
     <div className="app">
       <h1>Loja Online</h1>
-      <ProductList addToCart={addToCart} />
+      <ProductList addToCart={addToCart} showProductDetails={setShowDetails} />
       <Cart cartItems={cartItems} removeFromCart={removeFromCart} />
+      {showDetails && (
+        <div className="product-details-container">
+          <ProductList addToCart={addToCart} showProductDetails={setShowDetails} />
+        </div>
+      )}
     </div>
   );
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
-function ProductList({ addToCart }) {
+function ProductList({ addToCart, showProductDetails }) {
   const [products, setProducts] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:3001/products')
@@ -9,19 +10,48 @@ function ProductList({ addToCart }) {
       .then((data) => setProducts(data));
   }, []);
 
+  const handleProductClick = (product) => {
+    setSelectedProduct(product);
+    showProductDetails(true);
+  };
+
+  const handleBackClick = () => {
+    setSelectedProduct(null);
+    showProductDetails(false);
+  };
+
   return (
     <div className="product-list">
-      <h2>Produtos</h2>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <img src={product.image} alt={product.name} />
-            <h3>{product.name}</h3>
-            <p>Preço: R$ {product.price}</p>
-            <button onClick={() => addToCart(product)}>Adicionar ao Carrinho</button>
-          </li>
-        ))}
-      </ul>
+      {selectedProduct ? (
+        <div className="product-details">
+          <button onClick={handleBackClick}>Voltar</button>
+          <div className="product-details-content">
+            <div className="product-info">
+              <h3>{selectedProduct.name}</h3>
+              <p>Preço: R$ {selectedProduct.price.toFixed(2)}</p>
+              <p>{selectedProduct.description}</p>
+              <button onClick={() => addToCart(selectedProduct)}>Adicionar ao Carrinho</button>
+            </div>
+            <div className="product-image">
+              <img src={selectedProduct.image} alt={selectedProduct.name} />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h2>Produtos</h2>
+          <ul>
+            {products.map((product) => (
+              <li key={product.id}>
+                <img src={product.image} alt={product.name} />
+                <h3>{product.name}</h3>
+                <p>Preço: R$ {product.price.toFixed(2)}</p>
+                <button onClick={() => handleProductClick(product)}>Ver Detalhes</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
